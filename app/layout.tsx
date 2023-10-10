f19e21df462cff1056196e5804fc6aecc732b79d
Abdule Yousuf Hashi <114812664+abduleyousuf08@users.noreply.github.com>
2023-10-10 11:30:01 +0300

    Show the issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,7 @@ export default function RootLayout({
    return (
       <html lang='en'>
          <body className={inter.variable}>
-            <Theme
-               appearance='light'
-               accentColor='violet'
-               radius='large'
-               scaling='110%'
-            >
+            <Theme accentColor='violet' radius='large' scaling='110%'>
                <NavBar />
                <main className='p-5'>{children}</main>
             </Theme>
