850d43eef38d2fa55bec954dd8cda3d1153d5736
Abdule Yousuf Hashi <114812664+abduleyousuf08@users.noreply.github.com>
2023-10-11 14:01:09 +0300

    Add the edit button

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -30,7 +30,7 @@ async function IssuesPage() {
                      <Table.Cell>
                         <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
 
-                        <div className='block md:hidden'>
+                        <div className='block md:hidde mt-2'>
                            <IssueStatusBadge status={issue.status} />
                         </div>
                      </Table.Cell>
