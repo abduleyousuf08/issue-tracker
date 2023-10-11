64ff826d5305ed164c448dfb394bb80136e26817
Abdule Yousuf Hashi <114812664+abduleyousuf08@users.noreply.github.com>
2023-10-11 14:34:01 +0300

    Build edit issue page

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import IssueForm from '../../_components/IssueForm';
+import prisma from '@/prisma/client';
+import { notFound } from 'next/navigation';
 
-function EditIssuePage() {
-   return <div>EditIssuePage</div>;
+interface Props {
+   params: { id: string };
+}
+
+async function EditIssuePage({ params }: Props) {
+   const issue = await prisma.issue.findUnique({
+      where: { id: +params.id },
+   });
+
+   if (!issue) notFound();
+
+   return <IssueForm issue={issue} />;
 }
 
 export default EditIssuePage;
---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { createIssueSchema } from '@/app/ValidationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import 'easymde/dist/easymde.min.css';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+//*COMPONENTS
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Loader from '@/app/components/Loader';
+import { Issue } from '@prisma/client';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+   ssr: false,
+});
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+interface Props {
+   issue?: Issue;
+}
+
+function IssueForm({ issue }: Props) {
+   const router = useRouter();
+
+   //Todo: useForm hook setup
+   const {
+      register,
+      control,
+      handleSubmit,
+      formState: { errors },
+   } = useForm<IssueFormData>({
+      resolver: zodResolver(createIssueSchema),
+   });
+
+   //Todo:States
+   const [error, setError] = useState('');
+   const [isSubmiting, setIsSubmiting] = useState(false);
+
+   //Todo: to Submit an issue
+   const onSubmit = handleSubmit(async (data) => {
+      try {
+         setIsSubmiting(true);
+         await axios.post('/api/issues', data);
+         router.push('/issues');
+      } catch (error) {
+         setIsSubmiting(false);
+         setError('An expected error occured.');
+      }
+   });
+
+   return (
+      <div className='max-w-xl '>
+         {error && (
+            <Callout.Root color='red' className='mb-5'>
+               <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>
+         )}
+         <form className='max-w-xl space-y-3' onSubmit={onSubmit}>
+            <TextField.Root>
+               <TextField.Input
+                  defaultValue={issue?.title}
+                  placeholder='Title'
+                  {...register('title')}
+               />
+            </TextField.Root>
+
+            <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
+            <Controller
+               name='description'
+               control={control}
+               defaultValue={issue?.description}
+               render={({ field }) => (
+                  <SimpleMDE placeholder='Description...' {...field} />
+               )}
+            />
+
+            <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+            <Button disabled={isSubmiting}>
+               Submit New Issue {isSubmiting && <Loader />}
+            </Button>
+         </form>
+      </div>
+   );
+}
+
+export default IssueForm;
