2b2824ee32c9ae121596d888c270d3b8f5dab5bd
Abdule Yousuf Hashi <114812664+abduleyousuf08@users.noreply.github.com>
2023-10-28 11:01:29 +0300

    Set-up error trucker [ sentry ]

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://423f2edd58e29b991fb0f73704fa593c@o4506117351866368.ingest.sentry.io/4506117374541824",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
