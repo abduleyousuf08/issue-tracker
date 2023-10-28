1b01961f68d015c2e31cc1be46a9b3ad0cfb2899
Abdule Yousuf Hashi <114812664+abduleyousuf08@users.noreply.github.com>
2023-10-28 11:25:09 +0300

    Change relation mode to prisma

--- prisma/migrations/20231022084226_last_migration/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `issue` DROP FOREIGN KEY `Issue_assignedToUserId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `issue` ADD CONSTRAINT `issue_assignedToUserId_fkey` FOREIGN KEY (`assignedToUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
